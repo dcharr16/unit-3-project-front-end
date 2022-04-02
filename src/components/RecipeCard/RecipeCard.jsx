f75da14abda899becf0d2a532525207dc85e294e
dcharr16 <davidcraigharrison@gmail.com>
2022-04-02 18:04:55 -0400

    add pictures to recipe cards

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -1,9 +1,20 @@
-function RecipeCard({recipe}) {
+import {Link} from 'react-router-dom'
+function RecipeCard({recipe, randDishImgId}) {
   return(
     <div className="card">
+      <img 
+        src={`https://picsum.photos/id/${randDishImgId}/640/480`} 
+        alt="A happy puppy"
+        className="card-img-top" 
+      />
       <div className="card-body">
         <h2 className="card-text">{recipe.name}</h2>
         <p className="card-text"> {recipe.name} made with {recipe.ingredients} makes for a great {recipe.classification}</p>
+      </div>
+      <div className="card-footer">
+        <button className="btn btn-sm btn-danger m-left">
+          Delete
+        </button>
       </div>
 
     </div>
--- src/pages/RecipeList/RecipeList.jsx
+++ src/pages/RecipeList/RecipeList.jsx
@@ -1,13 +1,16 @@
 import styles from './RecipeList.module.css'
 import { RecipeCard } from '../../components/RecipeCard/RecipeCard'
 
+const dishIds = [102, 1080, 211, 225, 292, 326, 365, 429, 425, 431, 488, 493, 490, 824, 835, 889, 999]
+
 function RecipeList(props){
   return (
     <>
     <h1>Recipe List</h1>
     <div className={styles.container}>
       {props.recipes.map(recipe => (
-        <RecipeCard key={recipe._id} recipe= {recipe}/>
+        <RecipeCard key={recipe._id} recipe= {recipe}
+        randDishImgId = {dishIds[Math.floor(Math.random()*(dishIds.length))]}/>
         // <div key={recipe._id}>
         //   <p>Dish Name: {recipe.name}</p>
         //   <p>Ingredients:{recipe.ingredients}</p>
