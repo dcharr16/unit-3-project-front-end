855b08892e943f7faeeb47bd268e2097a3329474
dcharr16 <davidcraigharrison@gmail.com>
2022-04-03 12:28:22 -0400

    deploy to Heroku

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom'
 const NavBar = ({ user, handleLogout }) => {
   return (
     <>
-      {user ?
+    {user ?
+          
         <nav>
           <ul>
             <li>Welcome, {user.name}</li>
